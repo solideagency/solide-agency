2b4d7711c8e231b99a6b9c83a275a8f9339f2b5a
solideagency <suus.ten.voorde@hva.nl>
2020-05-28 16:31:11 +0200

    work page

--- pages/work.vue
+++ pages/work.vue
@@ -1,19 +1,28 @@
 <template>
   <div class="container">
     <div class="inner">
-      <h1>Work</h1>
+      <projectpreview/>
+      <calltoaction linkto="Contact"/>
     </div>
   </div>
 </template>
 
 <script>
+import { mapMutations } from 'vuex'
+import projectpreview from '~/components/projectpreview.vue'
+import calltoaction from '~/components/calltoaction.vue'
+
 export default {
   components: {
+    projectpreview,
+    calltoaction
   },
   async fetch ({store, params}) {
     return Promise.all([
       store.dispatch("company/getCompanyDetails"),
-      store.dispatch("socialmedia/getSocialMediaAccounts")
+      store.dispatch("socialmedia/getSocialMediaAccounts"),
+      store.dispatch("calltoaction/getCallToActions"),
+      store.dispatch("project/getProjects")
     ])
   },
   head () {
