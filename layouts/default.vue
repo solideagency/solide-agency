de2314ccadb36ac5bc9430b61bfe9f25965a269a
solideagency <suus.ten.voorde@hva.nl>
2020-06-01 17:34:01 +0200

    animation border-bottom teaser text

--- layouts/default.vue
+++ layouts/default.vue
@@ -55,17 +55,18 @@ export default {
   },
   mounted(){
     var switchButton = document.querySelector('.switch input');
-
     var bodyBackground = document.querySelector('body');
 
     switchButton.addEventListener('change', function(e){
       if(switchButton.checked === true){
         bodyBackground.classList.add("barcelona");
+        bodyBackground.classList.remove("amsterdam");
         document.querySelector('.city-images .amsterdam').style.display = "none";
         document.querySelector('.city-images .barcelona').style.display = "block";
 
       } else {
         bodyBackground.classList.remove("barcelona");
+        bodyBackground.classList.add("amsterdam");
         document.querySelector('.city-images .amsterdam').style.display = "block";
         document.querySelector('.city-images .barcelona').style.display = "none";
       }
