5b28f4bc358f0e9d5f179d48e85fbd71914d30c1
solideagency <suus.ten.voorde@hva.nl>
2020-06-02 15:50:15 +0200

    Not happy, but for now a solution to parallax image

--- components/cityimages.vue
+++ components/cityimages.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="city-images">
+  <section class="city-images rellax" data-rellax-speed="5">
     <div :class=(item.city.toLowerCase()) v-for="item in $store.state.cityimage.cityImages">
       <p class="caption">A creative agency in <span class="caption">{{item.city}}</span></p>
       <img :src="(item.image)">
