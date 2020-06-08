80f4dbd8bba8532f0cf15c49741bb537f33bb396
solideagency <suus.ten.voorde@hva.nl>
2020-06-08 14:10:08 +0200

    start of the flip ams bcn

--- pages/about-us.vue
+++ pages/about-us.vue
@@ -15,6 +15,45 @@
       <div class="two-places">
         <h1>We are in two places</h1>
         <p class="small-text" v-for="item in $store.state.company.companyDetails" v-html="item.culture"></p>
+        <div class="flip hidden">
+          <div class="nums nums-one">
+            <div class="num" data-num="A" data-num-next="P"></div>
+            <div class="num" data-num="P" data-num-next="S"></div>
+            <div class="num" data-num="S" data-num-next="D"></div>
+            <div class="num" data-num="D" data-num-next="W"></div>
+            <div class="num" data-num="W" data-num-next="R"></div>
+            <div class="num" data-num="R" data-num-next="J"></div>
+            <div class="num" data-num="J" data-num-next="G"></div>
+            <div class="num" data-num="G" data-num-next="R"></div>
+            <div class="num" data-num="R" data-num-next="K"></div>
+            <div class="num" data-num="K" data-num-next="A"></div>
+          </div>
+           <div class="nums nums-two">
+            <div class="num" data-num="M" data-num-next="O"></div>
+            <div class="num" data-num="O" data-num-next="L"></div>
+            <div class="num" data-num="L" data-num-next="N"></div>
+            <div class="num" data-num="N" data-num-next="B"></div>
+            <div class="num" data-num="B" data-num-next="V"></div>
+            <div class="num" data-num="V" data-num-next="E"></div>
+            <div class="num" data-num="E" data-num-next="T"></div>
+            <div class="num" data-num="T" data-num-next="M"></div>
+            <div class="num" data-num="M" data-num-next="A"></div>
+            <div class="num" data-num="A" data-num-next="M"></div>
+          </div>
+
+          <div class="nums nums-three">
+            <div class="num" data-num="S" data-num-next="G"></div>
+            <div class="num" data-num="G" data-num-next="W"></div>
+            <div class="num" data-num="W" data-num-next="F"></div>
+            <div class="num" data-num="F" data-num-next="I"></div>
+            <div class="num" data-num="I" data-num-next="X"></div>
+            <div class="num" data-num="X" data-num-next="A"></div>
+            <div class="num" data-num="A" data-num-next="C"></div>
+            <div class="num" data-num="C" data-num-next="Y"></div>
+            <div class="num" data-num="Y" data-num-next="Z"></div>
+            <div class="num" data-num="Z" data-num-next="S"></div>
+          </div>
+        </div>
       </div>
       <calltoaction linkto="Contact"/>
     </div>
@@ -41,6 +80,7 @@ export default {
     ])
   },
   mounted(){
+    // Toggle between employees
     var employees = document.querySelectorAll(".employee");
     var navigation = document.querySelectorAll('.more-about h3');
 
@@ -58,6 +98,37 @@ export default {
         employees[index].classList.add('active');
       })
     })
+
+    // Flip
+    function Flip() {
+      var elements;
+      var windowHeight;
+
+      function init() {
+        elements = document.querySelectorAll('.hidden');
+        windowHeight = window.innerHeight;
+      }
+
+      function checkPosition() {
+        for (var i = 0; i < elements.length; i++) {
+          var element = elements[i];
+          var positionFromTop = elements[i].getBoundingClientRect().top;
+
+          if (positionFromTop - windowHeight <= 0) {
+            element.classList.add('fade-in-element');
+            element.classList.remove('hidden');
+          }
+        }
+      }
+
+      window.addEventListener('scroll', checkPosition);
+      window.addEventListener('resize', init);
+
+      init();
+      checkPosition();
+    };
+
+    Flip();
 
   },
   head () {
