03fc7c283323f0f334be8c614b44fc73b993777e
solideagency <suus.ten.voorde@hva.nl>
2020-06-10 11:17:58 +0200

    test

--- layouts/default.vue
+++ layouts/default.vue
@@ -87,7 +87,7 @@ export default {
     // }
   },
   computed: {
-    routeName: async function(){
+    routeName: function(){
       console.log(this.$nuxt.$route.name)
       return this.$nuxt.$route.name
     }
