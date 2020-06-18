48d1c072408ee961fe368cb5f6fff8757cb590e8
solideagency <suus.ten.voorde@hva.nl>
2020-06-18 16:05:56 +0200

    google analytics how it works

--- nuxt.config.js
+++ nuxt.config.js
@@ -30,13 +30,15 @@ export default {
   ** Plugins to load before mounting the App
   */
   plugins: [
-    { src: '~plugins/ga.js', mode: 'client' },
     { src: '~/plugins/nuxt-client-init.js', ssr: false }
   ],
   /*
   ** Nuxt.js dev-modules
   */
   buildModules: [
+    ['@nuxtjs/google-analytics', {
+      id: process.env.GAID
+    }]
   ],
   /*
   ** Nuxt.js modules
